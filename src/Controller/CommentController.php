e30dff95d5480d5ab4982ecb495d5775eb546060
Raul <raul.bethencourt.pro@gmail.com>
2020-09-01 22:08:48 +0200

    :construction: service objects

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,14 +11,16 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}",name="app_comment_vote", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         // todo use id to query database
 
         // use real logic here to save this to database 
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
@@ -11,9 +10,13 @@ class QuestionController extends AbstractController
     /**
      * @Route("/",name="app_homepage")
      */
-    public function homepage()
+    public function homepage(/*Environment $twig*/)
     {
-        dump($this);
+        /*
+        fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+        */
 
         return $this->render('question/homepage.html.twig');
     }
